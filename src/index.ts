1723f60d3fcb53cabe59a8086e254a307a54d1f0
shahariarshawon <shahariararafat7739@gmail.com>
2026-02-12 16:47:06 +0600

    Problem 6 skipped and Problem 7 has been done

--- src/index.ts
+++ src/index.ts
@@ -39,3 +39,44 @@ function processValue(value: string | number): number {
 }
 console.log(processValue("hello"));
 console.log(processValue(10));
+
+// interface Product {
+//   name: string;
+//   price: number;
+// }
+
+// function getMostExpensiveProduct(products: Product[]): Product | null {
+//   if (products.length === 0) {
+//     return null;
+//   }
+//   let mostExpensiveProduct = products[0];
+//   for (let i = 1; i < products.length; i++) {
+//     if (products[i].price > mostExpensiveProduct.price) {
+//       mostExpensiveProduct = products[i];
+//     }
+//   }
+//   return mostExpensiveProduct;
+// }
+
+// const products = [
+//   { name: "Pen", price: 10 },
+//   { name: "Notebook", price: 25 },
+//   { name: "Bag", price: 50 },
+// ];
+
+// console.log(getMostExpensiveProduct(products));
+
+enum Day {
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday,
+  Sunday,
+}
+function getDayType(day: Day): string {
+  return day === Day.Saturday || day === Day.Sunday ? "Weekend" : "Weekday";
+}
+console.log(getDayType(Day.Monday));
+console.log(getDayType(Day.Saturday));
